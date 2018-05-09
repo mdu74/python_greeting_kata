2cdbd705722978ddd3dfa5b218a56717e2a284c5
Mdu <mdu74.C@gmail.com>
2018-05-09 21:46:57 +0200

    Check for null

--- greeting.py
+++ greeting.py
@@ -1,3 +1,6 @@
 class Greeting(object):
     def Greeter(name):
+        if name == None:
+            return "Hello, friend"
+        else:
             return "Hello, " + name
--- test_greeting.py
+++ test_greeting.py
@@ -2,7 +2,7 @@ import unittest
 from greeting import Greeting
 
 class TestGreeting(unittest.TestCase):
-    def test_Greeter_GivenSingleName_ShouldReturnGreetSingleName(self):
+    def test_Greeter_GivenSingleName_ShouldGreetSingleName(self):
         singleNames = ["Bob","Mary","Tom"]
         for eachName in singleNames:
             # Arrange
@@ -12,3 +12,12 @@ class TestGreeting(unittest.TestCase):
             # Assert
             expected = "Hello, " + eachName
             self.assertEqual(result, expected)
+    
+    def test_Greeter_GivenNull_ShouldReturnHelloFriend(self):
+        # Arrange
+        name = None
+        # Act
+        result = Greeting.Greeter(name)
+        # Assert
+        expected = "Hello, friend"
+        self.assertEqual(result, expected)
